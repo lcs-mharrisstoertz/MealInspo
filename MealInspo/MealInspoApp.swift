6d8412bb29566848c62d0f1eda7e642a4bf8c17f
Morgan Harris-Stoertz <mharrisstoertz@lcs.on.ca>
2023-11-01 12:13:49 -0400

    Initial Commit

---
+++ MealInspo/MealInspoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealInspoApp.swift
+//  MealInspo
+//
+//  Created by Morgan Harris-Stoertz on 2023-11-01.
+//
+
+import SwiftUI
+
+@main
+struct MealInspoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
